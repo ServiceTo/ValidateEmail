0cb19b8b2c86869f8bab1ff83ab834faccde8d9c
Ian Service <ian@service.to>
2016-08-14 21:28:02 -0400

    catch exceptions in the verification so we can return our exception

--- src/ValidateEmail.php
+++ src/ValidateEmail.php
@@ -70,52 +70,41 @@
 		 * @return boolean
 		 */
 		function verify($emailaddress, $mxhost) {
-			$validated = false;
+			try {
+				$validated = false;
 
-			$socket = stream_socket_client("tcp://" . $mxhost . ":25", $errno, $errstr, 30);
-			stream_set_blocking($socket, true);
+				$socket = stream_socket_client("tcp://" . $mxhost . ":25", $errno, $errstr, 30);
+				stream_set_blocking($socket, true);
 
-			$buffer = null;
-			// server will say hi...
-			$buffer .= fgets($socket, 2048);
+				$buffer = null;
+				// server will say hi...
+				$buffer .= fgets($socket, 2048);
 
-			$response = trim($buffer, "\r\n.");
-			$buffer = null;
-
-			list($code, $message) = preg_split("/\s+/", $response, 2);
-			if ($code == 220) {
-				// say hello.
-				$message = "EHLO ValidateEmail.service.to\r\n";
-				fwrite($socket, $message);
-
-				while($buf = fgets($socket)) {
-					$buffer .= $buf;
-					list($code, $message) = preg_split("/\s+/", $buf, 2);
-					if ($code == "250") {
-						break;
-					}
-				}
-				$response = trim($buffer, "\r\n");
+				$response = trim($buffer, "\r\n.");
 				$buffer = null;
 
-				$lines = preg_split("/\n/", $response);
-				$last = count($lines) - 1;
-				list($code, $message) = preg_split("/\s+/", $lines[$last], 2);
-				if ($code == 250) {
-					// give them my from address
-					$message = "MAIL FROM:<" . $this->testaddress . ">\r\n";
+				list($code, $message) = preg_split("/\s+/", $response, 2);
+				if ($code == 220) {
+					// say hello.
+					$message = "EHLO ValidateEmail.service.to\r\n";
 					fwrite($socket, $message);
 
-					$buffer .= fgets($socket, 2048);
-
+					while($buf = fgets($socket)) {
+						$buffer .= $buf;
+						list($code, $message) = preg_split("/\s+/", $buf, 2);
+						if ($code == "250") {
+							break;
+						}
+					}
 					$response = trim($buffer, "\r\n");
 					$buffer = null;
 
-					list($code, $message) = preg_split("/\s+/", $response, 2);
-
+					$lines = preg_split("/\n/", $response);
+					$last = count($lines) - 1;
+					list($code, $message) = preg_split("/\s+/", $lines[$last], 2);
 					if ($code == 250) {
-						// give them the user's address.
-						$message = "RCPT TO:<" . $emailaddress . ">\r\n";
+						// give them my from address
+						$message = "MAIL FROM:<" . $this->testaddress . ">\r\n";
 						fwrite($socket, $message);
 
 						$buffer .= fgets($socket, 2048);
@@ -124,24 +113,40 @@
 						$buffer = null;
 
 						list($code, $message) = preg_split("/\s+/", $response, 2);
+
 						if ($code == 250) {
-							$validated = true;
+							// give them the user's address.
+							$message = "RCPT TO:<" . $emailaddress . ">\r\n";
+							fwrite($socket, $message);
+
+							$buffer .= fgets($socket, 2048);
+
+							$response = trim($buffer, "\r\n");
+							$buffer = null;
+
+							list($code, $message) = preg_split("/\s+/", $response, 2);
+							if ($code == 250) {
+								$validated = true;
+							}
+
+							// say goodbye regardless
+							$message = "QUIT\r\n";
+							fwrite($socket, $message);
+
+							$buffer .= fgets($socket, 2048);
+
+							$response = trim($buffer, "\r\n");
+							$buffer = null;
+
+							list($code, $message) = preg_split("/\s+/", $response, 2);
+
+							return $validated;
 						}
-
-						// say goodbye regardless
-						$message = "QUIT\r\n";
-						fwrite($socket, $message);
-
-						$buffer .= fgets($socket, 2048);
-
-						$response = trim($buffer, "\r\n");
-						$buffer = null;
-
-						list($code, $message) = preg_split("/\s+/", $response, 2);
-
-						return $validated;
 					}
 				}
+			}
+			catch (Exception $e) {
+				throw new ValidateEmailException($e->toString());
 			}
 		}
 	}
